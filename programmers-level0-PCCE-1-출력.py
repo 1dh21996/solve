86ae06a0ad2f708182a40be7d77055f40926d510
HanDongHyeon <121152179+1dh21996@users.noreply.github.com>
2024-06-02 15:52:56 +0900

    solve PCCE-3, UPDATE PCCE-1, -2

--- programmers-level0-PCCE-1-출력.py
+++ programmers-level0-PCCE-1-출력.py
@@ -1,11 +1,8 @@
 
 # 빈칸 채우기 문제
-# 출력이 아래와 같도록 하는 변수를 입력
-# Spring is beginning
-# 13
-# 310
+# https://school.programmers.co.kr/learn/courses/30/lessons/250133
 
-string_msg = 'Spring is beginning'
+string_msg = "Spring is beginning"
 int_val = 3
 string_val = "3"
 
@@ -13,4 +10,4 @@ print(string_msg)
 print(int_val + 10)
 print(string_val + "10")
 
-# - 타입을 str(3) 이런 식으로 기재할 수도 있다는 것 지각
+# - 타입을 str(3) 이런 식으로 기재할 수도 있다는 것 알게 됐음
--- programmers-level0-PCCE-2 피타고라스의정리.py
+++ programmers-level0-PCCE-2 피타고라스의정리.py
@@ -1,13 +1,6 @@
 
 # [디버그 문제]
-# 직각삼각형 abc의 각 변의 관계는 a^2 + b^2 = C^2 과 같음
-#
-# 이 코드에서 1줄만 추가해서 코드를 수정할 것
-# a = int(input())
-# c = int(input())
-#
-# b_square = c - a
-# print(b_square)
+# https://school.programmers.co.kr/learn/courses/30/lessons/250132
 
 a = int(input())
 c = int(input())
