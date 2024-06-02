3ae27796224e26514de38f30d622189ee3b19ac8
HanDongHyeon <121152179+1dh21996@users.noreply.github.com>
2024-06-02 14:54:25 +0900

    solve PCCE-1

---
+++ programmers-level0-PCCE-1-출력.py
@@ -0,0 +1,16 @@
+
+# 빈칸 채우기 문제
+# 출력이 아래와 같도록 하는 변수를 입력
+# Spring is beginning
+# 13
+# 310
+
+string_msg = 'Spring is beginning'
+int_val = 3
+string_val = "3"
+
+print(string_msg)
+print(int_val + 10)
+print(string_val + "10")
+
+# - 타입을 str(3) 이런 식으로 기재할 수도 있다는 것 지각
